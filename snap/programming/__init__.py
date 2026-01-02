d89ea71bd1c4de30e1e25e46acbf8de9393b7408
Gordon Goodwin <gord.goodwin@gmail.com>
2026-01-02 14:26:15 -0800

    ...should have made sure it still ran before pushing change!

--- snap/programming/__init__.py
+++ snap/programming/__init__.py
@@ -5,7 +5,7 @@ def build(ENV):
 
 	ENV.__build__('snap.programming.compiling')
 	ENV.__build__('snap.programming.language')
-	ENV.__build__('snap.programming.project')
+	#ENV.__build__('snap.programming.project')
 
 	#ENV.__build__('snap.programming.SnapProject')
 
